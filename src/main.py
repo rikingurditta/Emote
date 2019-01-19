041d6f548e755504afc951767c6cee134736d656
Rikin <dolllyamar@gmail.com>
2019-01-19 07:38:06 -0500

    stuff works\!

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import cognitive_face as CF
+from api_keys import *
+
+CF.Key.set(API_KEY1)
+
+BASE_URL = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0'
+CF.BaseUrl.set(BASE_URL)
+
+# example image
+img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+faces = CF.face.detect(img_url)
+print(faces)
