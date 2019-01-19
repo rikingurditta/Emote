01cbe2347d7cb09c82f24353ea7b91e84828aa2c
itshannonk <39529318+itshannonk@users.noreply.github.com>
2019-01-19 10:12:58 -0500

    Dictionary w Emotion Definitions

---
+++ src/emotions_dict.py
@@ -0,0 +1,16 @@
+emotional_meaning = {
+    "anger": "A strong feeling of annoyance, displeasure, or hostility.",
+    "contempt": "The feeling that a person or a thing is worthless or beneath \
+     consideration.",
+    "disgust": "A feeling of revulsion or strong disapproval.",
+    "fear": "An unpleasant emotion caused by the threat of danger, pain, \
+     or harm.",
+    "happiness": "Feeling or showing pleasure or contentment.",
+    "neutral": "Having neither a positive nor negative emotion",
+    "sadness": "Feeling or showing sorrow; unhappy",
+    "surprise": "A feeling of mild astonishment or shock caused by \
+    something unexpected."
+}
+
+# Definitions from Oxford Dictionary
+# https://en.oxforddictionaries.com/
