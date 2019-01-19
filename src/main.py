0221aa2ec3ea6549467b4befb65b96bcc1a5978e
Rikin <dolllyamar@gmail.com>
2019-01-19 08:49:37 -0500

    removed api_keys cache, added emotion CF api call

--- src/main.py
+++ src/main.py
@@ -8,5 +8,5 @@ CF.BaseUrl.set(BASE_URL)
 
 # example image
 img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-faces = CF.face.detect(img_url)
+faces = CF.face.detect(img_url, attributes=['emotion'])
 print(faces)
