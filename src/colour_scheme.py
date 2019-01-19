846a2bddd01a941f46ece5e7bf367ea3242b4e1b
Sarina Wong <s.wong1218@gmail.com>
2019-01-19 18:51:16 -0500

    Colour Hex codes
    updated with colour_scheme hex codes

---
+++ src/colour_scheme.py
@@ -0,0 +1,9 @@
+colour_scheme
+fear_colour == 336699
+sadness_colour ==86bbd8
+anger_colour == 2f4858
+surprise_colour == 9ee493
+happy_colour == daf7dc
+neutral_colour == dce1df
+contempt_colour == 3ea7ba
+disgust == 39b599
